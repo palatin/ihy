a2fec43c3220256cf78f22787bbccc74405424bd
Ihor Shamin <ishamin@lohika.com>
2021-07-01 21:23:20 +0300

    Made roor as lazy vertical list

--- app/src/main/java/com/palatin/ihy/ui/screens/main/LibraryScreen.kt
+++ app/src/main/java/com/palatin/ihy/ui/screens/main/LibraryScreen.kt
@@ -3,10 +3,12 @@ package com.palatin.ihy.ui.screens.main
 import android.graphics.BlendMode
 import android.renderscript.RenderScript
 import android.renderscript.ScriptIntrinsicBlur
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
@@ -26,26 +29,42 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.palatin.ihy.ui.components.ContentCard
 import com.palatin.ihy.ui.components.SearchView
+import com.palatin.ihy.ui.components.SongDetail
 import com.palatin.ihy.ui.theme.Typography
 
 @Composable
 fun LibraryScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier
-        .verticalScroll(state = rememberScrollState())
+    LazyColumn(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color.White)
     ) {
-        Text(
-            text = "Library",
-            style = Typography.h4,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.fillMaxWidth()
-        )
-        SearchView(
-            text = "",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp)
-        )
-        RecentlyPlayed(listOf(Song("", ""), Song("", ""), Song("", "")))
+        item {
+            Text(
+                text = "Library",
+                style = Typography.h4,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth()
+            )
+            SearchView(
+                text = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            )
+        }
+        item {
+            RecentlyPlayed(listOf(Song("", ""), Song("", ""), Song("", "")))
+        }
+        item {
+            ContentSection()
+        }
+        items(listOf(Song("123", ""), Song("123", ""), Song("123", ""))) { song ->
+            SongDetail(
+                song = song
+            )
+        }
     }
 }
 
@@ -64,6 +83,11 @@ fun RecentlyPlayed(songs: List<Song>) {
             ContentCard()
         }
     }
+}
+
+// song or playlist
+@Composable
+fun ContentSection() {
 }
 
 @Preview(name = "Library light preview", device = Devices.PIXEL)
